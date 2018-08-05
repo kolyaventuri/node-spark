b1f12a4ce60c6746bc2755ccb224b14ebf850332
Kolya Venturi <kolya.venturi@enx3s.com>
2018-08-05 13:55:08 -0600

    Add API Service

--- src/api-service/index.js
+++ src/api-service/index.js
@@ -1,3 +1,20 @@
+import RequestBuilder from '../request-builder';
+import request from 'request-promise';
+
 export default class APIService {
-  
+  constructor(url, headers) {
+    this.builder = new RequestBuilder(url, headers);
+  }
+
+  async get(url, paramters) {
+    let opts = this.builder.build(url, parameters);
+
+    return await request(opts);
+  }
+
+  async post(url, body) {
+    let opts = this.builder.build(url, body, 'POST');
+
+    return await request(opts);
+  }
 }
