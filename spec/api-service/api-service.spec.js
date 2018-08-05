72896aba6e31bf6002cfe1490c08f0dcbdb934f4
Kolya Venturi <kolya.venturi@enx3s.com>
2018-08-05 13:49:26 -0600

    Add API service spec

---
+++ spec/api-service/api-service.spec.js
@@ -0,0 +1,24 @@
+import { APIService } from '../../src/api-service';
+
+describe('APIService', () => {
+  const API = new APIService('https://jsonplaceholder.typicode.com', {});
+
+  it('makes a get request', async () => {
+    const result = await API.get('/posts/1');
+
+    expect(result).to.be.an('object');
+  });
+
+  it('makes a post request', async () => {
+    const data = {
+      title: 'foo',
+      body: 'bar'
+    };
+
+    const result = await API.post('/posts', data);
+
+    expect(result).to.be.an('object')
+      .that.has.property('title')
+      .that.eqls(data.title);
+  });
+});
